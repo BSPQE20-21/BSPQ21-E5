82fc81dae58b80d35ad75ec882cac9290bf19aa4
johndavila8 <john.davila@opendeusto.es>
2021-04-29 08:43:42 +0200

    JUnit package and first try

---
+++ src/test/java/teses/testCliente.java
@@ -0,0 +1,14 @@
+package teses;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testCliente {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
