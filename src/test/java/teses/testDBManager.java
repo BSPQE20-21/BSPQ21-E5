4054ddfd1a4b5bfbb1256f5d3d442176d8f731ff
unaifj <unaifj@opendeusto.es>
2021-05-02 12:14:01 +0200

    clase test para la DB

---
+++ src/test/java/teses/testDBManager.java
@@ -0,0 +1,19 @@
+package teses;
+
+//import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+
+//import es.termibus.database.*;
+
+public class testDBManager {
+	
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+}
